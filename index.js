16739235b433a6ebfcc676c2bb7b4bd06e97eaef
limoh653 <limohesbon7@gmail.com>
2024-06-27 20:49:35 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee={
+    name:'man',
+
+}
+function updateEmployeeWithKeyAndValue(employee, streetAddress){
+
+    return{
+        ...employee, 
+        name: 'Sam' ,
+        streetAddress: '11 Broadway'  
+        
+    }}
+n
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, streetAddress){
+    employee.streetAddress="12 Broadway"
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee= { ...employee}
+    newEmployee[name]= undefined;
+    return newEmployee;
+    return employee;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    employee.name= undefined;
+    return employee;
+}
